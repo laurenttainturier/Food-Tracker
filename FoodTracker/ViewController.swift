7319dd0e39d91ea20a1b6912f8ea7743a5c724dd
laurenttainturier <laurent.tainturier.pro@gmail.com>
2020-07-07 17:37:04 +0200

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Laurent Tainturier on 07/07/2020.
+//  Copyright © 2020 Laurent Tainturier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
